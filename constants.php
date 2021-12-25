9eed082ce140aa0df77a79ac4f8384981cbcc1fb
Kartik Kumar <61470841+iamKaartik@users.noreply.github.com>
2021-12-25 17:11:24 +0530

    Second commit
    This has all the constants values.

---
+++ constants.php
@@ -0,0 +1,12 @@
+<?php
+//start session
+session_start();
+
+define('LOCALHOST',"localhost");
+define('DB_USERNAME',"root");
+define('DB_PASSWORD',"");
+define('DB_NAME','task_manager');
+
+define('SITEURL','http://localhost/Project_work/');
+
+?>
